c2b4c4caa3af09d48d2e9e2ffb6289b68443759f
Al-Saghir <ahmedalsaghirxyz@gmail.com>
2021-09-11 23:28:48 +0200

    Added tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: {
+    enabled: true,
+    content: ['./src/**/*.{html,scss,ts}']
+  },
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
