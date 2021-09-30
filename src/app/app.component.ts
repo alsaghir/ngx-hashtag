8f18c4f83842caa29480940d73af0e9b989fcc7d
Al-Saghir <ahmedalsaghirxyz@gmail.com>
2021-09-30 02:21:41 +0200

    Theme switch support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,7 @@
-import {Component, Inject, OnInit} from '@angular/core';
-import {DOCUMENT} from "@angular/common";
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: '<router-outlet></router-outlet>'
 })
-export class AppComponent implements OnInit{
-  layout = 'empty';
-
-  constructor(@Inject(DOCUMENT) private document: Document) {}
-
-  ngOnInit(): void {
-        this.loadStyle('default.css');
-    }
-
-
-  loadStyle(styleName: string) {
-    const head = this.document.getElementsByTagName('head')[0];
-
-    let themeLink = this.document.getElementById(
-      'client-theme'
-    ) as HTMLLinkElement;
-    if (themeLink) {
-      themeLink.href = styleName;
-    } else {
-      const style = this.document.createElement('link');
-      style.id = 'client-theme';
-      style.rel = 'stylesheet';
-      style.href = `${styleName}`;
-
-      head.appendChild(style);
-    }
-  }
-}
+export class AppComponent {}
--- src/app/module/landing/landing.component.ts
+++ src/app/module/landing/landing.component.ts
@@ -1,7 +1,81 @@
-import {Component} from '@angular/core';
+import {Component, Inject, OnDestroy, OnInit} from '@angular/core';
+import {Subject} from "rxjs";
+import {DOCUMENT} from "@angular/common";
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
 })
-export class LandingComponent {}
+export class LandingComponent implements OnInit, OnDestroy {
+
+  theme: 'dark' | 'default' = 'default';
+  private destroy$: Subject<void> = new Subject<void>();
+
+  constructor(@Inject(DOCUMENT) private document: Document) {}
+
+  ngOnInit(): void {
+    this.loadStyle(`${this.theme}.css`);
+  }
+
+  toggleTheme() {
+    if (this.theme === 'default') {
+      this.theme = 'dark';
+    } else {
+      this.theme = 'default';
+    }
+    // this.loadStyle(`${this.theme}.css`);
+    this.setStyle('theme', `${this.theme}.css`);
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  private loadStyle(styleName: string) {
+    const head = this.document.getElementsByTagName('head')[0];
+    let themeLink = this.document.getElementById('client-theme') as HTMLLinkElement;
+    if (themeLink) {
+      themeLink.href = styleName;
+    } else {
+      const style = this.document.createElement('link');
+      style.id = 'client-theme';
+      style.className = `style-manager-${this.theme}`;
+      style.rel = 'stylesheet';
+      style.href = `${styleName}`;
+
+      head.appendChild(style);
+    }
+  }
+
+  setStyle(key: string, href: string) {
+    this.getLinkElementForKey(key).setAttribute('href', href);
+  }
+
+  removeStyle(key: string) {
+    const existingLinkElement = this.getExistingLinkElementByKey(key);
+    if (existingLinkElement) {
+      this.document.head.removeChild(existingLinkElement);
+    }
+  }
+
+  private getLinkElementForKey(key: string): HTMLLinkElement {
+    return this.getExistingLinkElementByKey(key) ?? this.createLinkElementWithKey(key);
+  }
+
+  private getExistingLinkElementByKey(key: string): HTMLLinkElement | null {
+    return this.document.head.querySelector(`link[rel="stylesheet"].${LandingComponent.getClassNameForKey(key)}`);
+  }
+
+  private createLinkElementWithKey(key: string): HTMLLinkElement {
+    const linkEl = this.document.createElement('link');
+    linkEl.setAttribute('rel', 'stylesheet');
+    linkEl.classList.add(LandingComponent.getClassNameForKey(key));
+    this.document.head.appendChild(linkEl);
+    return linkEl;
+  }
+
+  private static getClassNameForKey(key: string): string {
+    return `style-manager-${key}`;
+  }
+}
